f8c76443b1d6d1fc9ab60d4a14f4c76d5570b7bf
Darlysson717 <darlysson717@example.com>
2026-02-24 19:51:50 -0300

    Fix: Prevent APK download on web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "342284226"
+    serviceWorkerVersion: "2096311632"
   }
 });
