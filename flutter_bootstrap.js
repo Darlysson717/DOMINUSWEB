f8c9eac4e44434fe1acfc9e3faeefe44fe2f51da
Darlysson717 <darlysson717@example.com>
2026-03-22 22:42:04 -0300

    Atualiza PWA

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2656971647"
+    serviceWorkerVersion: "2770925987"
   }
 });
